02d2ea0f28b973ccac6d013a85449b2d78289947
tip0x <adamsaddama@gmail.com>
2024-06-26 16:44:54 +0100

    updated repo

---
+++ MyTest.java
@@ -0,0 +1,8 @@
+public class MyTest
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello world!");
+        System.out.print("This is my first try in Java");
+    }
+}
